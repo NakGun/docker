7f4ded7dce68afb1736ed3c737e2edb43da3abdd
NakUn <chnaan@gmail.com>
2021-06-11 11:01:05 +0900

    agin

--- home/delete.php
+++ home/delete.php
@@ -5,3 +5,12 @@
 	$id = $_POST['id'];
 
 	$link->query("DELETE FROM users WHERE id = '".$id."'");
+
+	$query = $link->query("SELECT * FROM users");
+	$result = array();
+
+	while ($rowData = $query->fetch_assoc()) {
+		$result[] = $rowData;
+	}
+
+	echo json_encode($result);
